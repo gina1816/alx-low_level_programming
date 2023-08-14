47d3fe9b41b8113f62b9b697ef948a2492aec34f
Georgina Ohemeng <georginayebpah53@gmail.com>
2023-08-13 18:50:43 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - A program that prints a line with the puts function
+ * Return: 0(Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
