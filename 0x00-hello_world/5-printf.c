6f0d21041a85f50eeb8808ca26c5931ea54f7b5c
Georgina Ohemeng <georginayebpah53@gmail.com>
2023-08-13 19:12:26 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-printf("%s", "with proper grammar, but the outcome is a piece of art"): return(0):
+#include <stdio.h>
+/** 
+ * main - A function that priints a line with printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
