04682751fddf587ec73258a1560173a983393da6
Georgina Ohemeng <georginayebpah53@gmail.com>
2023-08-15 21:45:29 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _putchar(char c;
+
+void print_alphabet(void)
+{char alphabet = 'a';
+
+while (alphabet <= 'z'){
+_putchar(alphabet);
+alphabet++
+}
+
+_putchar('\n')
+
+}
